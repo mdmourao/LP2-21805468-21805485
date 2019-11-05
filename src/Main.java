24ca73b5949ab6c97bef0a49c3032d6a390912d2
Martim-21805485 <a21805485@alunos.ulht.pt>
2019-11-05 13:32:14 +0000

    app laucher a correr

--- src/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello");
-        System.out.println("mudei");
-        System.out.println("Ta tudo bem?");
-    }
-}
