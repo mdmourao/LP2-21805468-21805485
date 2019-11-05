da8ae66c6026feb03c01f1a449243f317a7860bf
Martim-21805485 <a21805485@alunos.ulht.pt>
2019-11-05 13:24:32 +0000

    a correr

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello");
         System.out.println("mudei");
+        System.out.println("Ta tudo bem?");
     }
 }
