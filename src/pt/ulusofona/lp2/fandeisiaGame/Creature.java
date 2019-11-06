905b966fbcb8773774747f6a4dc5656f5d0aee85
Martim-21805485 <a21805485@alunos.ulht.pt>
2019-11-06 10:29:37 +0000

    Defini as funções do enuciado e comentei as funções de acordo com o enuciado

--- src/pt/ulusofona/lp2/fandeisiaGame/Creature.java
+++ src/pt/ulusofona/lp2/fandeisiaGame/Creature.java
@@ -1,4 +1,19 @@
 package pt.ulusofona.lp2.fandeisiaGame;
 
 public class Creature {
+    public int getId(){
+        return 0;
+    }
+
+    public String getImagePNG(){
+        /*Deve devolver o nome do ficheiro de imagem (formato PNG) que representa a criatura.
+        Caso os alunos não pretendam definir nenhuma imagem, a função pode simplesmente retornar null. Isto fará com
+        que o visualizador use uma imagem pré-definida por omissão.*/
+        return null;
+    }
+
+    public String toString(){
+        return "";
+    }
 }
+
--- src/pt/ulusofona/lp2/fandeisiaGame/FandeisiaGameManager.java
+++ src/pt/ulusofona/lp2/fandeisiaGame/FandeisiaGameManager.java
@@ -1,4 +1,69 @@
 package pt.ulusofona.lp2.fandeisiaGame;
 
+import java.util.List;
+
 public class FandeisiaGameManager {
+
+    public String[][] getCreatureTypes() {
+        /*Deve retornar os tipos de criatura que existem no jogo e que podem ser
+        escolhidos para os exércitos dos dois jogadores.*/
+        return null;
+    }
+
+    public void startGame(String[] content, int rows, int columns) {
+       /* Deve inicializar as estruturas de dados relevantes para processar um jogo
+       O array content irá descrever o conteúdo inicial do mundo (criaturas e
+       tesouros), tendo para isso várias Strings. Cada String vai representar um objecto do mundo.
+       Os argumentos rows e columns vão-nos indicar as dimensões do tabuleiro.*/
+
+    }
+
+    public void setInitialTeam(int teamId) {
+        /*Indica qual das equipas vai jogar no primeiro turno do jogo.*/
+
+    }
+
+    public void processTurn() {
+        /*Deve processar um turno do jogo considerando a equipa actual. Inclui o movimento das criaturas.*/
+
+    }
+
+    public List<Creature> getCreatures() {
+        /*Devolve uma lista com todos os objectos Creature que existem no jogo.*/
+        return null;
+    }
+
+    public boolean gameIsOver() {
+        /*Deve devolver true caso já tenha sido alcançada uma das condições de paragem do jogo e false em caso contrário.*/
+        return false;
+    }
+
+    public List<String> getAuthors() {
+        /*Devolve uma lista de Strings com os nomes dos autores do projecto.*/
+        return null;
+    }
+
+    public List<String> getResults() {
+        /*Devolve uma lista de Strings que representem os resultados do jogo, conforme descrito na secção dos
+          “Resultados da execução …”. Este método não pode devolver null. Caso não calculem a informação
+           respectiva, devem devolver uma lista vazia.*/
+        return null;
+    }
+
+    public int getElementId(int x, int y) {
+        /*Deve devolver o ID do objecto/elemento que se encontra na posição indicada pelas coordenadas (x,y) passadas por
+          argumento.*/
+        return 0;
+    }
+
+    public int getCurrentTeamId() {
+        /*Deve devolver o ID da equipa que está activa no turno actual.*/
+        return 0;
+    }
+
+    public int getCurrentScore(int teamId) {
+        /*Deve devolver o número actual de pontos da equipa que tem o ID teamID.*/
+        return 0;
+    }
+
 }
