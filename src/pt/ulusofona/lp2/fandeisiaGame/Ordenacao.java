6cc27435dc0e63b602489220c671d13018d0a7ac
Martim-21805485 <a21805485@alunos.ulht.pt>
2019-11-09 19:09:30 +0000

    criação classe ordenacao criei file authors

---
+++ src/pt/ulusofona/lp2/fandeisiaGame/Ordenacao.java
@@ -0,0 +1,44 @@
+package pt.ulusofona.lp2.fandeisiaGame;
+
+public class Ordenacao {
+    private int partition(Creature[] creatures, int left, int right) {
+        // vamos escolher o pivot como sendo o elemento mais à direita
+        Creature pivot = creatures[right];
+        int leftIdx = left;
+        int rightIdx = right - 1; // exclude the pivot
+        while (leftIdx <= rightIdx) {
+            // se o left fôr maior que o pivot e o right fôr maior que o pivot, então troca
+            if (creatures[leftIdx].getId() > pivot.getId() && creatures[rightIdx].getId() < pivot.getId()) {
+                Creature temp = creatures[leftIdx];
+                creatures[leftIdx] = creatures[rightIdx];
+                creatures[rightIdx] = temp;
+            }
+            // se o left fôr menor que o pivot, avança
+            if (creatures[leftIdx].getId() <= pivot.getId()) {
+                leftIdx++;
+            }
+            // se o right fôr maior que o pivot, recua
+            if (creatures[rightIdx].getId() >= pivot.getId()) {
+                rightIdx--;
+            }
+        }
+        // troca pivot com left
+        creatures[right] = creatures[leftIdx];
+        creatures[leftIdx] = pivot;
+        return leftIdx;
+    }
+
+    private Creature[] ordenar(Creature[] creatures, int left, int right){
+        if(left < right){
+            int posicaoPivot = partition(creatures,left,right-1);
+
+            creatures = ordenar(creatures,left,posicaoPivot);
+            creatures = ordenar(creatures,posicaoPivot + 1,right);
+        }
+        return creatures;
+    }
+
+    public Creature[] ordenarCreatureById(Creature[] creatures){
+        return ordenar(creatures,0, creatures.length);
+    }
+}
