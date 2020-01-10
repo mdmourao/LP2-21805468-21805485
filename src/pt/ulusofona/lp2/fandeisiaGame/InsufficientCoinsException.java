353a8c6b2a56b4315c390a9be22fab3347e53afc
Gonçalo Évora <a21805468@alunos.ulht.pt>
2020-01-10 16:44:46 +0000

    Commit da classe InsufficientCoinsException

---
+++ src/pt/ulusofona/lp2/fandeisiaGame/InsufficientCoinsException.java
@@ -0,0 +1,7 @@
+package pt.ulusofona.lp2.fandeisiaGame;
+
+public class InsufficientCoinsException extends Exception{
+    public InsufficientCoinsException(String message) {
+        super(message);
+    }
+}
